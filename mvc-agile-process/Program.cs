635e79d3d9bb4915435044801623c92f53493c7c
Lucianna Mendonca <luciannamend@gmail.com>
2025-03-15 14:43:19 -0400

    Add changes from Part 5 and 6

--- mvc-agile-process/Program.cs
+++ mvc-agile-process/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using mvc_agile_process.Data;
+using mvc_agile_process.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<mvc_agile_processContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("mvc_agile_processContext") ?? throw new InvalidOperationException("Connection string 'mvc_agile_processContext' not found.")));
@@ -9,6 +11,13 @@ builder.Services.AddDbContext<mvc_agile_processContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
